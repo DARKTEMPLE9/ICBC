b6bea67fb83aa59581b453d9c20f6c6ac3746260
mxqer <171694512@qq.com>
2019-06-02 20:32:39 +0800

    提交新新项目

---
+++ power-shrio-log4j/src/main/java/com/icbc/IcbcApplication.java
@@ -0,0 +1,13 @@
+package com.icbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcbcApplication.class, args);
+    }
+
+}
